1db661956ddd4200b2ff27aba272b7ee382388a6
Yaodong Zhao <zhao@yaodong.org>
2017-05-15 15:13:14 -0600

    fix database uri

--- app/db.py
+++ app/db.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-engine = create_engine('postgresql://localhost/meep')
+engine = create_engine('postgresql://127.0.0.1/meep')
 
 Base = declarative_base()
 SessionBinding = sessionmaker(bind=engine)
